329640f124d14c5ab45603c25d44523caa62a844
samgun-6 <samuel.gunnarsson@gmail.com>
2019-11-19 14:50:14 +0100

    Adding project class started crating attributes and constructor but not ready

---
+++ src/main/java/se/turingturtles/Project.java
@@ -0,0 +1,12 @@
+package se.turingturtles;
+
+public class Project {
+
+    private String name;
+    private ArrayList<TeamMember> teamMembers;
+    private ArrayList<Task> tasksk;
+
+    public Project(String name){
+        this.name = name;
+    }
+}
