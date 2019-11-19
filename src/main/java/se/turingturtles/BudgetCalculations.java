cf1e58f86de9b075a1c328843e5bdb74f5e6d997
idhrenar <@a*a6aFDOKE\4kp;Ge:BwZCVg>
2019-11-19 16:00:36 +0100

    Added new methods to Budget Calculations.
    We worked on this together with @jeskanen.

--- src/main/java/se/turingturtles/BudgetCalculations.java
+++ src/main/java/se/turingturtles/BudgetCalculations.java
@@ -1,5 +1,56 @@
 package se.turingturtles;
 
+import java.util.List;
+
 public class BudgetCalculations {
+
+    private static final int UPDATE_INTERVAL = 14;
+
+
+
+
+    public double calculateEv(double completedWork, double completionBudget){
+
+
+
+        return 0;
+    }
+
+
+    public double calculateSV() {
+        //We calculate the Schedule Variance based on calculateBCWP & calculateBCWS
+    }
+
+    public double calculateCV(){
+        return 0;
+    }
+
+
+    public double calculateCompletedWorkPercentage(){
+        List<Tasks> tasks = project.getTasks();
+        int completedTasks = 0;
+
+        if (tasks.size() == 0){
+            return 0;
+        }
+
+        for (int i=0; i<tasks.size(); i++){
+            if (tasks.getStatus()) {
+                completedTasks++;
+            }
+        }
+
+        return completedTasks/tasks.size();
+    }
+
+    public double calculateBCWP(){
+        //Calculate Budget Cost of Work Performed
+    }
+
+    public double calculateBCWS(){
+        //Calculate Budgeted Cost of Work Scheduled
+    }
+
+    public
 
 }
