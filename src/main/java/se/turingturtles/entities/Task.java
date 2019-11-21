20782e22e28e9af4378dfebf97ce9f724388cfb1
SandraSmolerEisenberg <55246612+SandraSmolerEisenberg@users.noreply.github.com>
2019-11-21 16:10:46 +0100

    Added attributes, constructor and methods to Task Class

---
+++ src/main/java/se/turingturtles/entities/Task.java
@@ -0,0 +1,41 @@
+package se.turingturtles.entities;
+
+import java.util.ArrayList;
+
+public class Task {
+
+    private String name;
+    private int duration; //number of weeks
+    private ArrayList<TeamMember> teamMembers;
+
+    public Task(String name, int duration){
+        this.name = name;
+        this.duration = duration;
+        this.teamMembers = new ArrayList<TeamMember>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public int getDuration(){
+        return this.duration;
+    }
+    public void setDuration(int newDuration){
+        this.duration = newDuration;
+    }
+    public ArrayList<TeamMember> getTeamMembers(){
+        return teamMembers;
+    }
+    public void addTeamMember(TeamMember teamMember){
+        this.teamMembers.add(teamMember);
+    }
+    public void removeTeamMember(TeamMember teamMember){
+        this.teamMembers.remove(teamMember);
+    }
+    public int TotalTeamMembers(){
+        return teamMembers.size();
+    }
+}
