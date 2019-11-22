ef5fc940ba0dcd16bad1694eacb45f6cc145d68c
SandraSmolerEisenberg <55246612+SandraSmolerEisenberg@users.noreply.github.com>
2019-11-22 13:46:20 +0100

    Added attributes, constructor and methods to Risk class

--- src/main/java/se/turingturtles/entities/Risk.java
+++ src/main/java/se/turingturtles/entities/Risk.java
@@ -1,4 +1,37 @@
 package se.turingturtles.entities;
 
 public class Risk {
+
+    private String name;
+    private int impact;
+    private int probability;
+
+    public Risk(String name, int impact, int probability){
+        this.name = name;
+        this.impact = impact;
+        this.probability = probability;
+    }
+    //--------------------Getters & Setters--------------------
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public int getImpact(){
+        return this.impact;
+    }
+    public void setImpact(int newImpact){
+        this.impact = newImpact;
+    }
+    public int getProbability(){
+        return this.probability;
+    }
+    public void setProbability(int newProbability){
+        this.probability = newProbability;
+    }
+    //--------------------Methods--------------------
+    public double calculateRisk(){
+        return (this.impact * this.probability);
+    }
 }
