4e5e826c287c18c1857304d9fabb2d1f9ada85ee
Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>
2019-11-26 00:28:26 +0100

    Additions in TeamPageController class and modifications in the respective teampage.fxml file.
    When clicking on "Add new member" the respective AnchorPane changes its visibility to true, in order for the form to appear.
    
    Data input validation added.

--- src/main/java/se/turingturtles/controllers/TeamPageController.java
+++ src/main/java/se/turingturtles/controllers/TeamPageController.java
@@ -3,16 +3,24 @@ package se.turingturtles.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import se.turingturtles.ProjectManagement;
-import se.turingturtles.entities.Project;
+import se.turingturtles.Validator;
 import se.turingturtles.entities.TeamMember;
 import se.turingturtles.implementations.ProjectFactory;
 import se.turingturtles.implementations.ProjectManagementImp;
+
+import java.awt.event.MouseEvent;
 
 public class TeamPageController {
 
@@ -25,22 +33,63 @@ public class TeamPageController {
     @FXML
     private TextField enterWage;
     @FXML
-    public Button createMemberButton;
+    protected Button createMemberButton;
     @FXML
-    public Button refresh;
+    private AnchorPane newMemberPage;
+    @FXML
+    protected Button returnTeamPage;
 
-    public void loadTeamList(ActionEvent loadEvent) {
+    public void loadTeamList() {
 
         ObservableList<TeamMember> members = FXCollections.observableArrayList(ProjectManagementImp.getProject().getTeamMembers());
         teamList.setItems(members);
 
     }
     public void createNewMember(ActionEvent event){
+        newMemberPage.setVisible(true);
+        String name = enterName.getText();
+        String id = enterID.getText();
+        String hourlyWage = enterWage.getText();
         ProjectFactory factory = new ProjectFactory();
-        ProjectManagement projectManagement = factory.createProjectManagement();
-        projectManagement.createMember(enterName.getText(), Integer.parseInt(enterID.getText()),Double.parseDouble(enterWage.getText()));
-        enterName.clear();
-        enterWage.clear();
-        enterID.clear();
+        Validator validator = factory.makeValidator();
+
+        if (validator.validateTextInput(name) && validator.validateNumericInput(id) && validator.validateNumericInput(hourlyWage)) {
+            ProjectManagement projectManagement = factory.createProjectManagement();
+            projectManagement.createMember(name, Integer.parseInt(id),Double.parseDouble(hourlyWage));
+            enterName.clear();
+            enterWage.clear();
+            enterID.clear();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("New team member creation was successful!");
+            alert.setHeaderText("You have created a new team member successfully!");
+            alert.setContentText("A new team member has been created with the following information: \nName: " + name + "\nID: " + id + "\nHourly Wage: " + hourlyWage + "\n\nPlease click OK, in order to proceed!");
+            alert.showAndWait();
+
+            loadTeamList();
+        } else {
+            if (!validator.validateTextInput(name)) {
+                enterName.clear();
+                enterName.setPromptText("Invalid Name!");
+                //Color to be changed
+                enterName.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+            }
+            if (!validator.validateNumericInput(id)) {
+                enterID.clear();
+                enterID.setPromptText("Invalid ID!");
+                //Color to be changed
+                enterID.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+            }
+            if (!validator.validateNumericInput(hourlyWage)) {
+                enterWage.clear();
+                enterWage.setPromptText("Invalid Wage!");
+                //Color to be changed
+                enterWage.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+            }
+        }
+    }
+
+    public void returnToTeamPage() {
+        newMemberPage.setVisible(false);
     }
 }
