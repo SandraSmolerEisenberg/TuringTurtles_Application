68baa2cd8b35093946cf52e47a05194c81021347
Eemil Jeskanen <gusjesee@student.gu.se>
2019-11-25 17:37:59 +0100

    Working on teampage fxml file and teampage controller, added methods for creation for a new member. Worked on this with @chrtsa

--- src/main/java/se/turingturtles/controllers/TeamPageController.java
+++ src/main/java/se/turingturtles/controllers/TeamPageController.java
@@ -4,28 +4,43 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.ListCell;
-
-import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TextField;
+import se.turingturtles.ProjectManagement;
+import se.turingturtles.entities.Project;
 import se.turingturtles.entities.TeamMember;
+import se.turingturtles.implementations.ProjectFactory;
 import se.turingturtles.implementations.ProjectManagementImp;
 
 public class TeamPageController {
 
     @FXML
     private ListView teamList;
+    @FXML
+    private TextField enterName;
+    @FXML
+    private TextField enterID;
+    @FXML
+    private TextField enterWage;
+    @FXML
+    public Button createMemberButton;
+    @FXML
+    public Button refresh;
 
-
-    public void setTeamList(Event mouseEvent) {
+    public void loadTeamList(ActionEvent loadEvent) {
 
         ObservableList<TeamMember> members = FXCollections.observableArrayList(ProjectManagementImp.getProject().getTeamMembers());
         teamList.setItems(members);
 
     }
+    public void createNewMember(ActionEvent event){
+        ProjectFactory factory = new ProjectFactory();
+        factory.createTeamMember(enterName.getText(), Integer.parseInt(enterID.getText()),Double.parseDouble(enterWage.getText()));
+        enterName.clear();
+        enterWage.clear();
+        enterID.clear();
+    }
+
 }
