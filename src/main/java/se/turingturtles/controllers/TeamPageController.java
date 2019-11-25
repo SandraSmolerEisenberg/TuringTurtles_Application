054cb86626aa74cdf8b27aa885e95274064f6aa1
Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>
2019-11-25 14:42:27 +0100

    Addition of a ListView in TeamPage, and also fixed a bug in the respective TeamPage controller.
    Worked with @jeskanen

--- src/main/java/se/turingturtles/controllers/TeamPageController.java
+++ src/main/java/se/turingturtles/controllers/TeamPageController.java
@@ -1,4 +1,13 @@
 package se.turingturtles.controllers;
 
+import javafx.fxml.FXML;
+
+//import javax.swing.text.html.ListView;
+import javafx.scene.control.ListView;
+
 public class TeamPageController {
+
+    @FXML
+    private ListView teamList;
+
 }
