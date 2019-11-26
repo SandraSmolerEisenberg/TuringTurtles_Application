39ea4e252eaf1b17d8ce5ba830819f7002d3cd4e
Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>
2019-11-26 23:34:34 +0100

    Minor changes in TeamPageController and teampage.fxml, in order to fix the error message showing by default in the fields without completing anything. Also, the fields' color and values are being reverted to the initial field stage (empty and white background), no matter of the user's previous input (whether it was valid or invalid).

--- src/main/java/se/turingturtles/controllers/TeamPageController.java
+++ src/main/java/se/turingturtles/controllers/TeamPageController.java
@@ -45,13 +45,18 @@ public class TeamPageController {
         teamList.setItems(members);
 
     }
-    public void createNewMember(ActionEvent event){
+
+    public void showNewMemberCreation(ActionEvent e) {
         newMemberPage.setVisible(true);
+    }
+
+    public void createNewMember(ActionEvent event){
         String name = enterName.getText();
         String id = enterID.getText();
         String hourlyWage = enterWage.getText();
         ProjectFactory factory = new ProjectFactory();
         Validator validator = factory.makeValidator();
+
 
         if (validator.validateTextInput(name) && validator.validateNumericInput(id) && validator.validateNumericInput(hourlyWage)) {
             ProjectManagement projectManagement = factory.createProjectManagement();
@@ -68,28 +73,45 @@ public class TeamPageController {
 
             loadTeamList();
         } else {
-            if (!validator.validateTextInput(name)) {
-                enterName.clear();
-                enterName.setPromptText("Invalid Name!");
-                //Color to be changed
-                enterName.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
-            }
-            if (!validator.validateNumericInput(id)) {
-                enterID.clear();
-                enterID.setPromptText("Invalid ID!");
-                //Color to be changed
-                enterID.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
-            }
-            if (!validator.validateNumericInput(hourlyWage)) {
-                enterWage.clear();
-                enterWage.setPromptText("Invalid Wage!");
-                //Color to be changed
-                enterWage.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+            //With the above if statement we make sure that in case of empty values, the fields will not show the error messages.
+            if (!(name.equals("") && id.equals("") && hourlyWage.equals(""))) {
+                if (!validator.validateTextInput(name)) {
+                    enterName.clear();
+                    enterName.setPromptText("Invalid Name!");
+                    //Color to be changed
+                    enterName.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+                }
+                if (!validator.validateNumericInput(id)) {
+                    enterID.clear();
+                    enterID.setPromptText("Invalid ID!");
+                    //Color to be changed
+                    enterID.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+                }
+                if (!validator.validateNumericInput(hourlyWage)) {
+                    enterWage.clear();
+                    enterWage.setPromptText("Invalid Wage!");
+                    //Color to be changed
+                    enterWage.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+                }
             }
         }
     }
 
     public void returnToTeamPage() {
         newMemberPage.setVisible(false);
+
+        //We clear the fields in case of incorrect values entered the last time, so that the previous values will not appear again.
+        enterName.clear();
+        enterWage.clear();
+        enterID.clear();
+
+        /*We set the name and the background color of the fields to the default ones, in case of invalid user input data, so they will not
+        appear the next time again. */
+        enterName.setPromptText("Enter name");
+        enterName.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        enterID.setPromptText("Enter ID");
+        enterID.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        enterWage.setPromptText("Enter hourly wage");
+        enterWage.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
     }
 }
