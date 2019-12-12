70150ecb8a05750a1b3955217e5f8da7ab5f2ad2
krasen <krasen@student.chalmers.se>
2019-12-12 17:11:51 +0100

    Added method to Start page

--- src/main/java/se/turingturtles/controllers/StartPageController.java
+++ src/main/java/se/turingturtles/controllers/StartPageController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import se.turingturtles.Main;
+import se.turingturtles.ProjectManagement;
 import se.turingturtles.implementations.ProjectFactory;
 import se.turingturtles.streamIO.StreamJSON;
 
@@ -20,6 +21,7 @@ public class StartPageController {
 
 
     ProjectFactory factory = new ProjectFactory();
+    ProjectManagement projectManagement = factory.makeProjectManagement();
 
     public void createNewProject(ActionEvent event) throws IOException {
         factory.changeScene(createProjectButton.getScene(),"createproject");
@@ -28,6 +30,7 @@ public class StartPageController {
     public void loadProject(ActionEvent event) throws Exception{
             StreamJSON testStream = factory.makeStream();
             testStream.importFromJSON();
+            projectManagement.triggerCalculations();
             factory.changeScene(createProjectButton.getScene(),"projectmaster");
 
     }
