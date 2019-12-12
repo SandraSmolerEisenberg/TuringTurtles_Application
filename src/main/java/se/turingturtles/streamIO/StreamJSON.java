a00bf578e689644f6dc255a255a80cd14b9e7c50
Chrysostomos Tsagkidis <chrtsa@student.chalmers.se>
2019-12-12 14:42:44 +0100

    New version with the Task bug worked with @jeskanen @krasen

--- src/main/java/se/turingturtles/streamIO/StreamJSON.java
+++ src/main/java/se/turingturtles/streamIO/StreamJSON.java
@@ -2,10 +2,12 @@ package se.turingturtles.streamIO;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import javafx.scene.control.Alert;
 import se.turingturtles.entities.Project;
 import se.turingturtles.implementations.ProjectFactory;
 import se.turingturtles.implementations.ProjectManagementImp;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,10 +20,15 @@ public class StreamJSON {
     public StreamJSON(){
         ProjectFactory factory = new ProjectFactory();
         mapper = factory.makeObjectMapper();
+
+
     }
 
     public void exportToJSON() {
         try {
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            mapper.registerModule(new JavaTimeModule());
+            mapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             mapper.writeValue(new File(JSON_FILE_PATH), ProjectManagementImp.getProject());
         }catch(IOException e){
             Alert exportError = new Alert(Alert.AlertType.ERROR);
@@ -35,6 +42,8 @@ public class StreamJSON {
     public void importFromJSON() {
         try {
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            mapper.registerModule(new JavaTimeModule());
+            mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
             ProjectManagementImp.setProject(mapper.readValue(new File(JSON_FILE_PATH), Project.class));
         }catch (IOException e){
             Alert importError = new Alert(Alert.AlertType.ERROR);
