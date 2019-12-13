2356b35a22e4f744b41b8e73ed3c4912e2bd970d
liivmarii <liv.alterskjaer@icloud.com>
2019-12-13 20:18:40 +0100

    Added button to go back to start page above tab-pane.

--- src/main/java/se/turingturtles/controllers/ProjectMasterController.java
+++ src/main/java/se/turingturtles/controllers/ProjectMasterController.java
@@ -1,10 +1,15 @@
 package se.turingturtles.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import se.turingturtles.implementations.ProjectFactory;
+
+import java.io.IOException;
 
 
 public class ProjectMasterController {
@@ -21,10 +26,17 @@ public class ProjectMasterController {
     @FXML private Tab teamMembersTab;
     @FXML private Tab riskTab;
     @FXML private Tab projectManagementTab;
+    @FXML private Button startPageButton;
+
+    private ProjectFactory factory = new ProjectFactory();
 
     public void initialize(){
        projectMasterTabs.getStylesheets().add(getClass().getResource("/se/turingturtles/css/00-default.css").toExternalForm());
 
+    }
+
+    public void backToStartPage(ActionEvent actionEvent) throws IOException {
+        factory.changeScene(startPageButton.getScene(),"startPage");
     }
 
 }
